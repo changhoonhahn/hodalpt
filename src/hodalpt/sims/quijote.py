b5d5cbc93da3ab2a107b2974429383980d9f514b
ChangHoon Hahn <changhoon.hahn@utexas.edu>
2025-11-18 15:37:29 -0600

    added function to quickly apply RSD on HOD or halo boxes

--- src/hodalpt/sims/quijote.py
+++ src/hodalpt/sims/quijote.py
@@ -391,3 +391,24 @@ def _fname_format(snapshot):
         raise Exception('File (%s) not found!' % snapshot)
     return filename,fformat
 
+
+def Box_RSD(cat, LOS=[0,0,1], Lbox=1000.):
+    ''' Given a halo/galaxy catalog in a periodic box, apply redshift space
+    distortion specified LOS along LOS
+
+    Parameters
+    ----------
+    cat : CatalogBase
+        nbodykit.Catalog object
+    LOS : array_like
+        3 element list specifying the direction of the line-of-sight
+    Lbox : float
+        box size in Mpc/h
+    '''
+    pos = np.array(cat['Position']) + np.array(cat['VelocityOffset']) * LOS
+
+    # impose periodic boundary conditions for particles outside the box
+    i_rsd = np.arange(3)[np.array(LOS).astype(bool)][0]
+    rsd_pos = pos[:,i_rsd] % Lbox
+    pos[:,i_rsd] = np.array(rsd_pos)
+    return pos
