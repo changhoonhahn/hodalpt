0546dab75f8c34bcf2f564b77c2cba1785e5477c
ChangHoon Hahn <changhoon.hahn@utexas.edu>
2026-03-04 15:57:14 -0600

    added rockstar halos to globus file transfer

--- bin/data_transfer/globus_quijote_transfer.sh
+++ bin/data_transfer/globus_quijote_transfer.sh
@@ -9,7 +9,7 @@ LONESTAR_ENDPOINT="fc2bdfc1-168f-4a55-8264-084b8c1b646c"
 
 globus session consent 'urn:globus:auth:scope:transfer.api.globus.org:all[*https://auth.globus.org/scopes/fc2bdfc1-168f-4a55-8264-084b8c1b646c/data_access]'
 
-globus transfer $QUIJOTE_ENDPOINT:/Snapshots/latin_hypercube_HR/ \
+globus transfer $QUIJOTE_ENDPOINT:/ \
                 $LONESTAR_ENDPOINT:/corral/utexas/AST25023/simbig/quijote/latinhypercube_hr \
                 --batch transfer_manifest.txt \
                 --label "quijote initial conditions" \
--- bin/data_transfer/make_manifest.py
+++ bin/data_transfer/make_manifest.py
@@ -3,19 +3,29 @@ import os
 import numpy as np 
 
 ff = open('transfer_manifest.txt', 'w')
-for i in [78, 79]+list(np.arange(81, 100)): #range(2000): 
-    ff.write('%i/CAMB.params %i/CAMB.params\n' % (i, i))
-    ff.write('%i/Cosmo_params.dat %i/Cosmo_params.dat\n' % (i, i))
-    ff.write('%i/ICs/2LPT.param %i/ICs/2LPT.param\n' % (i,i))
-    ff.write('%i/ICs/ics.0.hdf5 %i/ICs/ics.0.hdf5\n' % (i,i))
-    ff.write('%i/ICs/ics.1.hdf5 %i/ICs/ics.1.hdf5\n' % (i,i))
-    ff.write('%i/ICs/ics.2.hdf5 %i/ICs/ics.2.hdf5\n' % (i,i))
-    ff.write('%i/ICs/ics.3.hdf5 %i/ICs/ics.3.hdf5\n' % (i,i))
-    ff.write('%i/ICs/ics.4.hdf5 %i/ICs/ics.4.hdf5\n' % (i,i))
-    ff.write('%i/ICs/ics.5.hdf5 %i/ICs/ics.5.hdf5\n' % (i,i))
-    ff.write('%i/ICs/ics.6.hdf5 %i/ICs/ics.6.hdf5\n' % (i,i))
-    ff.write('%i/ICs/ics.7.hdf5 %i/ICs/ics.7.hdf5\n' % (i,i))
-    ff.write('%i/ICs/inputspec_ics.txt %i/ICs/inputspec_ics.txt\n' % (i,i))
-    ff.write('%i/ICs/Pk_mm_z=0.000.txt %i/ICs/Pk_mm_z=0.000.txt\n' % (i,i))
+for i in range(100, 200):
+    ff.write('Snapshots/latin_hypercube_HR/%i/CAMB.params %i/CAMB.params\n' % (i, i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/Cosmo_params.dat %i/Cosmo_params.dat\n' % (i, i))
+
+    # initial conditions 
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/2LPT.param %i/ICs/2LPT.param\n' % (i,i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/ics.0.hdf5 %i/ICs/ics.0.hdf5\n' % (i,i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/ics.1.hdf5 %i/ICs/ics.1.hdf5\n' % (i,i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/ics.2.hdf5 %i/ICs/ics.2.hdf5\n' % (i,i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/ics.3.hdf5 %i/ICs/ics.3.hdf5\n' % (i,i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/ics.4.hdf5 %i/ICs/ics.4.hdf5\n' % (i,i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/ics.5.hdf5 %i/ICs/ics.5.hdf5\n' % (i,i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/ics.6.hdf5 %i/ICs/ics.6.hdf5\n' % (i,i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/ics.7.hdf5 %i/ICs/ics.7.hdf5\n' % (i,i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/inputspec_ics.txt %i/ICs/inputspec_ics.txt\n' % (i,i))
+    ff.write('Snapshots/latin_hypercube_HR/%i/ICs/Pk_mm_z=0.000.txt %i/ICs/Pk_mm_z=0.000.txt\n' % (i,i))
+    
+    # rockstar halo catalogs
+    #ff.write('Halos/Rockstar/latin_hypercube_HR//%i/out_4_pid.list %i/Rockstar/out_4_pid.list\n' % (i, i))
+    ff.write('Halos/Rockstar/latin_hypercube_HR//%i/out_3_pid.list %i/Rockstar/out_3_pid.list\n' % (i, i))
+    #ff.write('Halos/Rockstar/latin_hypercube_HR//%i/out_2_pid.list %i/Rockstar/out_2_pid.list\n' % (i, i))
+    #ff.write('Halos/Rockstar/latin_hypercube_HR//%i/out_1_pid.list %i/Rockstar/out_1_pid.list\n' % (i, i))
+    #ff.write('Halos/Rockstar/latin_hypercube_HR//%i/out_0_pid.list %i/Rockstar/out_0_pid.list\n' % (i, i))
+
 ff.close() 
 
