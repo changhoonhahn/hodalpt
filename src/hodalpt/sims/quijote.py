fed0aea00eff9cfbcde2935288eb1dbe37b40df4
ChangHoon Hahn <changhoon.hahn@utexas.edu>
2026-05-11 10:55:03 -0500

    fixed minor restriction on naming

--- src/hodalpt/sims/quijote.py
+++ src/hodalpt/sims/quijote.py
@@ -229,7 +229,7 @@ class Snap(object):
 def _cosmo_lookup(setup, ireal): 
     ''' look up cosmology for quijote realization 
     '''
-    if setup not in ['fiducial', 'latin_hypercube_HR']: 
+    if setup.lower() not in ['fiducial', 'latin_hypercube_hr', 'latinhypercube_hr']: 
         raise NotImplementedError("non-fiducial cosmologies not yet implemented")
 
     if 'fiducial' in setup: 
