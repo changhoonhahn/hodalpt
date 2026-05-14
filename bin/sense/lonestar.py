1562428ab8bb5eecf54258f7ebb4d20f98ec816a
mcc1200 <mcasas@utexas.edu>
2026-05-14 13:17:14 -0500

    updated HOD prior to Gaussian via SimBIG I CMASS BOSS posterior

--- bin/sense/lonestar.py
+++ bin/sense/lonestar.py
@@ -46,6 +46,7 @@ def run_alpt_sobol(i0, i1, time=1, queue='development', silent=True):
         else: 
             print('%i already complete' % i_lhc) 
 
+
     # create the script.sh file, execute it and remove it
     f = open(os.path.join(os.environ['WORK'], 'script.slurm'),'w')
     f.write(a)
@@ -102,11 +103,7 @@ def run_quijote(i0, i1, time=1, queue='development', silent=True):
         ''])
     
     for i_lhc in range(i0, i1): 
-        if not check_alpt_runs(i_lhc): 
-            a += "python %s/quij_test.py %i %s\n" % (scriptdir, i_lhc, _dir)) 
-        else: 
-            print('%i already complete' % i_lhc) 
-
+        a += "python %s/quij_test.py %i %s\n" % (scriptdir, i_lhc, _dir) 
     # create the script.sh file, execute it and remove it
     f = open(os.path.join(os.environ['WORK'], 'script.slurm'),'w')
     f.write(a)
@@ -121,5 +118,8 @@ if __name__=="__main__":
     #run_alpt_sobol(0, 50, queue='development', time=1)
 
     #run_alpt_sobol(0, 1, queue='development', time=0.5, silent=False)
-    run_quijote(0, 1, time=0.5, queue='development', silent=True):
-
+    run_quijote(0, 1, time=0.5, queue='development', silent=True)
+    # run_quijote(1, 100, time=1, queue='normal')
+    # run_quijote(100, 200, time=1, queue='normal')
+    # run_quijote(200, 300, time=1, queue='normal')
+    # run_quijote(300, 400, time=1, queue='normal')
