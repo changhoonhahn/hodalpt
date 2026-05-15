28816d7ba530b6a116b8bc5e5c73429237ab2232
mcc1200 <mcasas@utexas.edu>
2026-05-15 12:52:55 -0500

    uploading thetas, naming convention fix

--- bin/sense/gen_pip_thetas.py
+++ bin/sense/gen_pip_thetas.py
@@ -4,7 +4,7 @@ from hodalpt.sims import sampling as S
 
 
 outfile_NLB = 'NLB_samples_10000.hdf5'
-outfile_HOD = 'HOD_samps_1000.hdf5'
+outfile_HOD = 'HOD_samples_1000.hdf5'
 
 S.generate_and_save_NLB(outfile_NLB, 10000)
 S.generate_and_save_HOD(outfile_HOD, 1000)
