b4490d3f6b0d6d362e157fb235ce4e7c29f2d66a
mcc1200 <mcasas@utexas.edu>
2026-05-19 16:39:50 -0500

    check if spec exists

--- bin/sense/bispec_fid.py
+++ bin/sense/bispec_fid.py
@@ -22,6 +22,9 @@ def load_pos(specfn):
 for i in range(i0,i1):
     t0 = time.time()
     specfn = os.path.join(NLB_dir, 'spec.%i.h5' %i)
+    if not os.path.isfile(specfn):
+        print('MISSING %i' % i)
+        continue
     xyz = load_pos(specfn)
     bispec = stats.B0_periodic(xyz.T, w=None, Lbox=1000., fft='pyfftw', silent=True)
     with h5py.File(specfn, 'a') as f:
@@ -32,16 +35,19 @@ for i in range(i0,i1):
             f['b123'] = bispec['b123']
             f['q123'] = bispec['q123']
     print('bispectrum added for NLB sample %i' % i)
-    if i < 1000: 
+    if i < 1000:
         specfn = os.path.join(HOD_dir, 'spec.%i.h5' %i)
-        xyz = load_pos(specfn)
-        bispec = stats.B0_periodic(xyz.T, w=None, Lbox=1000., fft='pyfftw', silent=True)
-        with h5py.File(specfn, 'a') as f:
-            if 'b123' not in f:
-                f['i_k1'] = bispec['i_k1']
-                f['i_k2'] = bispec['i_k2']
-                f['i_k3'] = bispec['i_k3']
-                f['b123'] = bispec['b123']
-                f['q123'] = bispec['q123']
-        print('bispectrum added for HOD sample %i' % i)
+        if not os.path.isfile(specfn):
+            print('MISSING HOD %i' % i)
+        else:
+            xyz = load_pos(specfn)
+            bispec = stats.B0_periodic(xyz.T, w=None, Lbox=1000., fft='pyfftw', silent=True)
+            with h5py.File(specfn, 'a') as f:
+                if 'b123' not in f:
+                    f['i_k1'] = bispec['i_k1']
+                    f['i_k2'] = bispec['i_k2']
+                    f['i_k3'] = bispec['i_k3']
+                    f['b123'] = bispec['b123']
+                    f['q123'] = bispec['q123']
+            print('bispectrum added for HOD sample %i' % i)
     print('bias sample %i finished in %.1f s' % (i, time.time() - t0))
